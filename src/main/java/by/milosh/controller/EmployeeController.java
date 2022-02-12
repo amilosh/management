99997db68a70e364b03e53c806218fe6818cabeb
Alexander Milosh <milosh.alexander.by@gmail.com>
2022-02-12 10:30:59 +0300

    Add lombok logging

--- src/main/java/by/milosh/controller/EmployeeController.java
+++ src/main/java/by/milosh/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import by.milosh.entity.EmployeeCreateDto;
 import by.milosh.entity.EmployeeDto;
 import by.milosh.mapper.EmployeeMapper;
 import by.milosh.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/employee")
 public class EmployeeController implements EmployeeControllerApi {
@@ -28,7 +30,9 @@ public class EmployeeController implements EmployeeControllerApi {
     @PostMapping(consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDto createEmployee(@RequestBody EmployeeCreateDto dto) {
+        log.debug("Starting to create Employee");
         Employee employee = employeeService.create(dto);
+        log.info("Employee was created");
         return employeeMapper.toDto(employee);
     }
 }
